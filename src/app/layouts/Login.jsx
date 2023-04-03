9213d6bd3e44b3dad4f1f27eef397d5d816c86f8
Aleksander <asava@sibmail.com>
2023-04-03 22:50:27 +0700

    Обработка значений множества полей

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -3,33 +3,42 @@ import React, { useState } from 'react'
 // form>(div>label+input)*2
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const handleChange = (e) => {
-    setEmail(e.target.value)
-    console.log(e.target.value)
+  // const [email, setEmail] = useState('') // можно так дя каждого поля свое состояние
+  // const [password, setPassword] = useState('')
+  const [data, setData] = useState({ email: '', password: '' })
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value
+      }))
+    // console.log(e.target.value)
   }
-// d первом случае мы можем получит доступ к значениям и ролучать события об обновление поля
-  // На данный момент поле "Емаил" является контролируемым с точки зрения реакта
-  // потому что мы можем получять изменнения и указыват значения
 
-// во втором случае это поле сущ. само по себе в дереве элементов браузера
-  // Второе "password" не контролируемое
+  // Получаем предыдущие состояние "prevState" в ответ возвращаем объект,
+  // который будет состоять из предыдущего состояния. И нам необходимо изменить данные нашего поля.
+  // Для этого мы обращаемся к полю как [e.target.name]: и записываем данные из event.target.value
 
   return (
     <form action="">
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          id='email'
-          value={email}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Пароль</label>
-        <input type="password" id='password' />
-      </div>
+        <div>
+            <label htmlFor="email">Email</label>
+            <input
+                type="text"
+                id='email'
+                name='email'
+                value={data.email}
+                onChange={handleChange}
+            />
+        </div>
+        <div>
+            <label htmlFor="password">Пароль</label>
+            <input
+                type="password"
+                id='password'
+                name='password'
+                value={data.password}
+                onChange={handleChange}/>
+        </div>
     </form>
   )
 }
