c97ba53aa9b1c2837499fbfae3e728a01fc4b51f
Aleksander <asava@sibmail.com>
2023-04-03 23:40:30 +0700

    ok

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -16,7 +16,6 @@ const Login = () => {
     <form action="">
         <TextField
             label='Электронная почта'
-            // type="text" // передали его подефолту в PropTypes в TextField
             name='email'
             value={data.email}
             onChange={handleChange}
@@ -28,25 +27,6 @@ const Login = () => {
             value={data.password}
             onChange={handleChange}
           />
-        {/* <div>
-            <label htmlFor="email">Email</label>
-            <input
-                type="text"
-                id='email'
-                name='email'
-                value={data.email}
-                onChange={handleChange}
-            />
-        </div>
-        <div>
-            <label htmlFor="password">Пароль</label>
-            <input
-                type="password"
-                id='password'
-                name='password'
-                value={data.password}
-                onChange={handleChange}/>
-        </div> */}
     </form>
   )
 }
