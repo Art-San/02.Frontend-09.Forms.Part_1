1d4c507373f3a08e9a55efeed0ce9459d3565c7c
Aleksander <asava@sibmail.com>
2023-04-03 23:32:31 +0700

    3. Обработка значений множества полей

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -1,34 +1,26 @@
 import React, { useState } from 'react'
-import TextField from '../components/TextField'
 
 // form>(div>label+input)*2
 
 const Login = () => {
+  // const [email, setEmail] = useState('') // можно так дя каждого поля свое состояние
+  // const [password, setPassword] = useState('')
   const [data, setData] = useState({ email: '', password: '' })
   const handleChange = ({ target }) => {
     setData((prevState) => ({
         ...prevState,
         [target.name]: target.value
       }))
+    // console.log(e.target.value)
   }
+
+  // Получаем предыдущие состояние "prevState" в ответ возвращаем объект,
+  // который будет состоять из предыдущего состояния. И нам необходимо изменить данные нашего поля.
+  // Для этого мы обращаемся к полю как [e.target.name]: и записываем данные из event.target.value
 
   return (
     <form action="">
-        <TextField
-            label='Электронная почта'
-            // type="text" // передали его подефолту в PropTypes в TextField
-            name='email'
-            value={data.email}
-            onChange={handleChange}
-          />
-          <TextField
-            label='Пароль'
-            type="password"
-            name='password'
-            value={data.password}
-            onChange={handleChange}
-          />
-        {/* <div>
+        <div>
             <label htmlFor="email">Email</label>
             <input
                 type="text"
@@ -46,7 +38,7 @@ const Login = () => {
                 name='password'
                 value={data.password}
                 onChange={handleChange}/>
-        </div> */}
+        </div>
     </form>
   )
 }
