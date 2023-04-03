9c8c0e6f8bfbd859545f89b8f5e94380cca47f55
Aleksander <asava@sibmail.com>
2023-04-03 20:55:49 +0700

    Создание формы

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+// form>(div>label+input)*2
 
 const Login = () => {
   return (
-    <h1>LoginPage</h1>
+    <form action="">
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" id='email' />
+      </div>
+      <div>
+        <label htmlFor="password">Пароль</label>
+        <input type="password" id='password' />
+      </div>
+    </form>
   )
 }
 
