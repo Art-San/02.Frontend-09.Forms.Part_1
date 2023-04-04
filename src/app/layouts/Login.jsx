201324fc35ebc2978eebc6512b642b61bd2f94a4
Aleksander <asava@sibmail.com>
2023-04-04 13:50:06 +0700

    Базовая валидация

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -1,19 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TextField from '../components/TextField'
 
 // form>(div>label+input)*2
 
 const Login = () => {
     const [data, setData] = useState({ email: '', password: '' })
+    const [errors, setErrors] = useState('')
+    console.log('errors', errors)
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }))
     }
+    useEffect(() => {
+        validate()
+    }, [data])
+
+    const validate = () => {
+        const errors = {}
+        for (const fieldName in data) {
+            if (data[fieldName].trim() === '') {
+                errors[fieldName] = `${fieldName} обязательно для заполнения`
+            }
+        }
+        setErrors(errors)
+        return Object.keys(errors).length !== 0 // Базовая валидация var2
+    }
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('data', data)
+        // validate() // Базовая валидация var1
+        // if (Object.keys(errors).length !== 0) return var1
+        const isValid = validate() // Базовая валидация var2
+        if (!isValid) return // Базовая валидация var2
+        console.log(data) // Базовая валидация var2
     }
     return (
         <form onSubmit={handleSubmit}>
@@ -31,7 +51,6 @@ const Login = () => {
                 onChange={handleChange}
             />
             <button>Отправить</button>
-            <button type="button">Другое действие</button>
         </form>
     )
 }
